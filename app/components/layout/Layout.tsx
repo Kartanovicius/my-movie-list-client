2313d45465cfa7fdb140df2cddd3c10fa26c3d32
donataskartanovicius <donatas.karta@gmail.com>
2022-08-15 20:00:58 +0300

    feat: use meta component instead of just head

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,20 +1,18 @@
-import Head from 'next/head';
 import { FC, PropsWithChildren } from 'react';
 
 import Header from '@components/layout/header/Header';
 import Sidebar from '@components/layout/sidebar/Sidebar';
 
+import Meta from '@utils/Meta';
+
 import style from './Layout.module.scss';
 
-const Layout: FC<PropsWithChildren<{ title: string }>> = ({
-	title,
-	children,
-}) => {
+const Layout: FC<
+	PropsWithChildren<{ title: string; description?: string }>
+> = ({ title, description, children }) => {
 	return (
 		<div>
-			<Head>
-				<title>{title}</title>
-			</Head>
+			<Meta title={title} description={description} />
 			<main className={style.main}>
 				<Sidebar />
 				<section className={style.content}>
