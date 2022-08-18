1b452636281f91685103cdc499c87750feb12e67
donataskartanovicius <donatas.karta@gmail.com>
2022-08-18 23:18:06 +0300

    test: create for field ui component

---
+++ app/components/ui/field/__stories__/Field.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import * as React from 'react';
+
+import Field from '../Field';
+
+export default {
+	title: 'Components/Field',
+	component: Field,
+	argTypes: {
+		label: {
+			control: { type: 'text' },
+		},
+		placeholder: {
+			control: { type: 'text' },
+		},
+		type: {
+			control: { type: 'text' },
+		},
+		error: {
+			control: { type: 'object' },
+		},
+	},
+} as ComponentMeta<typeof Field>;
+
+const Template: ComponentStory<typeof Field> = args => <Field {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	placeholder: 'Default Text Input Field',
+};
+
+export const Label = Template.bind({});
+Label.args = {
+	label: 'Label',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+	error: { message: 'Error message!' },
+};
