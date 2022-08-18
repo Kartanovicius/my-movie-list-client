6ceab55db88f9168a6dec68bca904c9638c9890d
donataskartanovicius <donatas.karta@gmail.com>
2022-08-18 21:39:41 +0300

    test: create for button ui component

---
+++ app/components/ui/button/__stories__/Button.stories.tsx
@@ -0,0 +1,87 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import * as React from 'react';
+
+import Button, { ButtonIcon } from '../Button';
+
+export default {
+	title: 'Components/Button',
+	component: Button,
+	argTypes: {
+		children: {
+			control: { type: 'text' },
+		},
+		className: {
+			control: { type: 'text' },
+		},
+		disabled: {
+			control: { type: 'boolean' },
+		},
+		fullWidth: {
+			control: { type: 'boolean' },
+		},
+		rounded: {
+			options: ['regular', 'extra'],
+			control: { type: 'radio' },
+		},
+		size: {
+			options: ['small', 'medium', 'large'],
+			control: { type: 'radio' },
+		},
+	},
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
+
+const TemplateWithIcon: ComponentStory<typeof Button> = ({
+	children,
+	className,
+	fullWidth,
+	rounded,
+	size,
+}) => (
+	<Button
+		className={className}
+		size={size}
+		rounded={rounded}
+		fullWidth={fullWidth}
+	>
+		<ButtonIcon>🐑</ButtonIcon>
+		{children}
+	</Button>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+	children: 'Default Button',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	children: 'Disabled',
+	disabled: true,
+};
+
+export const ButtonFullWidth = Template.bind({});
+ButtonFullWidth.args = {
+	children: 'Default Button',
+	fullWidth: true,
+};
+
+export const ButtonAlignText = Template.bind({});
+ButtonAlignText.args = {
+	children: 'Align text with justify',
+	className: 'justify-end',
+	fullWidth: true,
+};
+
+export const ButtonWithIcon = TemplateWithIcon.bind({});
+ButtonWithIcon.args = {
+	children: 'Icon Button',
+};
+
+export const GenreButton = TemplateWithIcon.bind({});
+GenreButton.args = {
+	children: `Genre`,
+	size: 'large',
+	rounded: 'extra',
+};
