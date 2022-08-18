7fc9d4e9eb59dba0619b4bdee4f0b5fca159ed65
donataskartanovicius <donatas.karta@gmail.com>
2022-08-18 21:09:50 +0300

    ci: add controls and tailwind.css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,13 @@
+import 'tailwindcss/tailwind.css';
+
 import '../app/styles/globals.scss';
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 };
