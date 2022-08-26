f15794e3cdbd499c4341b48815c74f6734b0d4c9
donataskartanovicius <donatas.karta@gmail.com>
2022-08-26 16:42:24 +0300

    fix: remove unused character ;

---
+++ app/store/api/api.ts
@@ -0,0 +1,32 @@
+import { API_URL } from '@api/axios';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { IUser } from 'types/user.interface';
+
+import { TypeRootState } from '@store/store';
+
+import { USER } from '@utils/constants';
+
+export const api = createApi({
+	reducerPath: 'api',
+	tagTypes: ['Show', 'Profile', 'DatabaseFiles'],
+	baseQuery: fetchBaseQuery({
+		baseUrl: API_URL,
+		prepareHeaders: (headers, { getState }) => {
+			const token = (getState() as TypeRootState).auth.accessToken;
+
+			if (token) headers.set('Authorization', `Bearer ${token}`);
+
+			return headers;
+		},
+	}),
+	endpoints: builder => ({
+		getProfile: builder.query<IUser, any>({
+			query: () => `${USER}/profile`,
+			providesTags: () => [{ type: 'Profile' }],
+		}),
+		getProfileById: builder.query<IUser, number>({
+			query: id => `${USER}/by-id/${id}`,
+			providesTags: () => [{ type: 'Profile' }],
+		}),
+	}),
+});
