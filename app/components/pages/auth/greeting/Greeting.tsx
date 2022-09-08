633e8bf09ea488e663e7f93775be10d1434b88e2
donataskartanovicius <donatas.karta@gmail.com>
2022-09-08 22:49:54 +0300

    refactor: improve classNames and edit project_name

--- app/components/pages/auth/greeting/Greeting.tsx
+++ app/components/pages/auth/greeting/Greeting.tsx
@@ -9,11 +9,18 @@ const Greeting: FC = () => {
 	const router = useRouter();
 
 	return (
-		<div className={styles.greeting_container}>
-			<div className={styles.greeting}>
-				<h2 onClick={() => router.push('/')}>{PROJECT_NAME}</h2>
-				<h1>Looking for movie recommendations?</h1>
-				<p>
+		<div className={styles.greeting}>
+			<div className={styles.greeting_container}>
+				<h2
+					className={styles.greeting_container_project_name}
+					onClick={() => router.push('/')}
+				>
+					{PROJECT_NAME}
+				</h2>
+				<h1 className={styles.greeting_container_title}>
+					Looking for movie recommendations?
+				</h1>
+				<p className={styles.greeting_container_description}>
 					We have you covered. Check our latest Movie recommendation and find
 					movie or Tv Series just for you.
 				</p>
