5786ed8cec8c3411b1cd8c96ea2304140681578c
donataskartanovicius <donatas.karta@gmail.com>
2022-09-20 17:40:19 +0200

    feat: create Discover element in MainPage

--- app/components/pages/home/discovery/Discover.tsx
+++ app/components/pages/home/discovery/Discover.tsx
@@ -1,7 +1,75 @@
 import { FC } from 'react';
+import ContentLoader from 'react-content-loader';
+import { AiFillStar } from 'react-icons/ai';
+
+import MovieLargeCard, {
+	MovieLargeCardAdditionalInfo,
+	MovieLargeCardAdditionalInfoItem,
+	MovieLargeCardDescription,
+	MovieLargeCardTitle,
+} from '@components/ui/cards/movie-large-card/MovieLargeCard';
+
+import { showApi } from '@store/api/show.api';
+
+import { textConverter, toTitleCase } from '@utils/textConverter';
+
+import styles from './Discover.module.scss';
 
 const Discover: FC = () => {
-	return <div>Discover</div>;
+	const { data: lastAddedShow, isLoading: loadingLastAddedShow } =
+		showApi.useGetLastAddedShowQuery();
+	if (!lastAddedShow) return null;
+
+	return (
+		<div className={styles.discover}>
+			{loadingLastAddedShow && (
+				<ContentLoader
+					backgroundOpacity={0.1}
+					foregroundOpacity={0.15}
+					style={{ width: '100%', height: '300px', aspectRatio: '16/9' }}
+				>
+					<rect
+						x='0'
+						y='0'
+						rx='10'
+						ry='10'
+						style={{ width: '100%', height: '100%' }}
+					/>
+				</ContentLoader>
+			)}
+			{!loadingLastAddedShow && (
+				<MovieLargeCard
+					showId={lastAddedShow.id}
+					img={{
+						src: lastAddedShow.posterPath,
+						alt: 'Movie image',
+					}}
+					className={'md:h-48'}
+				>
+					<MovieLargeCardTitle>{lastAddedShow.name}</MovieLargeCardTitle>
+					<MovieLargeCardDescription>
+						{lastAddedShow.description}
+					</MovieLargeCardDescription>
+					<MovieLargeCardAdditionalInfo>
+						<MovieLargeCardAdditionalInfoItem>
+							{textConverter(lastAddedShow.duration)}
+						</MovieLargeCardAdditionalInfoItem>
+						<MovieLargeCardAdditionalInfoItem>
+							{lastAddedShow.genre.map((genre, index) =>
+								index == 0 ? toTitleCase(genre) : `, ${toTitleCase(genre)}`,
+							)}
+						</MovieLargeCardAdditionalInfoItem>
+						<MovieLargeCardAdditionalInfoItem>
+							<span className={'flex-center gap-1'}>
+								{lastAddedShow.rating}
+								<AiFillStar className={'text-orange-400'} />
+							</span>
+						</MovieLargeCardAdditionalInfoItem>
+					</MovieLargeCardAdditionalInfo>
+				</MovieLargeCard>
+			)}
+		</div>
+	);
 };
 
 export default Discover;
---
+++ app/utils/textConverter.ts
@@ -0,0 +1,19 @@
+export function toTitleCase(str: string) {
+	return str.replace(/\w\S*/g, function (txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	});
+}
+
+export function textConverter(totalMinutes: number | undefined) {
+	if (totalMinutes === undefined) return null;
+	const minutes = totalMinutes % 60;
+	const hours = Math.floor(totalMinutes / 60);
+
+	return `${
+		hours > 9 ? padTo2Digits(hours) + 'h' : hours > 0 ? hours + 'h' : ''
+	} ${padTo2Digits(minutes)}min`;
+}
+
+function padTo2Digits(num: number) {
+	return num.toString().padStart(2, '0');
+}
